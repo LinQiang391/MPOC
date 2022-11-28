3ac996f17ef17b077bb5d22bf84e972983b7a189
LinQiang391 <1667704220@qq.com>
2022-11-29 00:33:51 +0800

    增加了客户端与服务端通过单个或者多个socket通信的功能

--- Util/socketutils.py
+++ Util/socketutils.py
@@ -16,14 +16,20 @@ class Socket:
     def send_message(self, connection, msg):
         msg = msg.encode('utf-8')
         head = struct.pack('i', len(msg))  # 把消息长度打包成固定长度的bytes
-        connection.send(head)  # 发送包头
-        connection.send(msg)  # 发送消息
+        try:
+            connection.send(head)  # 发送包头
+            connection.send(msg)
+        except Exception as e:
+            print(f'send message failed,exceptiom as {e}')# 发送消息
 
         # 获取消息，处理粘包
 
     def get_message(self, connection):
-        head = connection.recv(4)
-        length = struct.unpack('i', head)[0]  # 把包头解包成int类型
+        try:
+            head = connection.recv(4)
+            length = struct.unpack('i', head)[0]
+        except Exception as e:
+            return None
         # 开始读取长度为 length 的数据
         recData = b''  # 获取到的消息
         cacheLen = 1024  # 缓冲区大小
@@ -44,11 +50,24 @@ class Socket:
     def getPort(self):
         return self.__port
 
-    def create_socket(self, bind: bool = True):
+    def create_socket(self, bind: bool = True, host: str = None, port: int = None):
+        if host is None:
+            host = self.__host
+        if port is None:
+            port = self.__port
         connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if bind:
-            connection.bind((self.getHost(), self.getPort()))
+            connection.bind((host, port))
+        else:
+            connection.connect((host, port))
+
         return connection
+
+    def send_close_message(self, connection: socket = None):
+        if connection is None:
+            return
+        else:
+            self.send_message(connection, 'close')
 
 
 class Server(Socket):
@@ -56,24 +75,74 @@ class Server(Socket):
     def __init__(self, host: str = '127.0.0.1', port: int = 3333):
         super(Server, self).__init__(host, port)
 
-    def server_start(self):
+    #单个socket完成消息客户端与服务端消息的交互，只允许与单个客户端通信，绑定相同个端口
+    def server_start_with_single_socket(self):
         s = self.create_socket()
         s.listen()
+        connection, address = s.accept()
+        while True:
+            try:
+                message = self.get_message(connection=connection)
+                if message is not None:
+                    if message == 'close':
+                        connection.close()
+                        s.close()
+                        return
+                    else:
+                        print(message)
+            except Exception as e:
+                print(f'occur exception {e}')
+                connection.close()
+                s.close()
+                return
+        connection.close()
+        s.close()
+
+    def server_start_with_differ_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('127.0.0.1', 3333))
+        s.listen()
+        num = 0
+        # connection, address =s.accept()
+        l = []
         while True:
             try:
                 connection, address = s.accept()
-                message = self.get_message(connection=connection)
-                if message == 'close':
-                    connection.close()
-                    s.close()
-                    break
+                data = self.get_message(connection)
+                if data == 'close':
+                    pass
+                    # self.__redis.close()
+                    #self.send_quit_message()
                 else:
-                    connection.close()
-                    print(message)
+                    print(data)
+            except Exception as e:
+                print('occur exception as {}'.format(e))
+                break
+
+    def server_start(self, connection: socket = None, single: bool = True):
+        if connection is None:
+            connection = self.create_socket(bind=True)
+        connection.listen()
+
+        if single:
+            new_connection, address = connection.accept()
+        while True:
+            try:
+                if not single:
+                    new_connection, address = connection.accept()
+                message = self.get_message(connection=new_connection)
+                if message is not None:
+                    if message == 'close':
+                        new_connection.close()
+                        connection.close()
+                        return
+                    else:
+                        print(message)
             except Exception as e:
                 print(f'occur exception {e}')
-                s.close()
-                break
+                new_connection.close()
+                connection.close()
+                return
 
 
 class Client(Socket):
@@ -81,15 +150,23 @@ class Client(Socket):
     def __init__(self, host: str = '127.0.0.1', port: int = 3333):
         super(Client, self).__init__(host, port)
 
-    def send_message_num(self, n):
-        for i in range(n):
-            s = self.create_socket(bind=False)
-            try:
-                s.connect((self.getHost(), self.getPort()))
-            except Exception as e:
-                print('occur exception {}'.format(e))
-            rand = random.randint(1, 10)
-            data_res = "insert into tbl values {}".format(rand)
-            self.send_message(s, data_res)
-            print(f"{i}发送数据成功{data_res}")
-            s.close()
+    def send_message_from_list(self, connection: socket = None, msgs: list = None, close: bool = True) -> bool:
+        if connection is None:
+            for msg in msgs:
+                connection = self.create_socket(bind=False)
+                try:
+                    self.send_message(connection, msg)
+                    print(msg)
+                except Exception as e:
+                    print(f'occur exception as {e}')
+                    return False
+                connection.close()
+        else:
+            for msg in msgs:
+                try:
+                    self.send_message(connection,msg)
+                except Exception as e:
+                    return False
+            if close:
+                connection.close
+        return True
--- client.py
+++ client.py
@@ -1,111 +1,25 @@
-# -*- coding:utf-8 -*-
-import socket
-import struct
-import random
-import threading
 import time
 from Util.socketutils import Client
+import threading
 
-# class Client:
-#     __host = None
-#     __port = None
-#     dic ={'1':'lin','2':'li','3':'cao','4':'ji','5':'wu','6':'han','7':'fei','8':'mo','9':'lan','10':'cai'}
-#
-#     def __init__(self):
-#         self.__host = '127.0.0.1'
-#         self.__port = 5555
-#
-#     def send_message(self, n):
-#         #data_res = ''
-#         for i in range(n):
-#             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#             try:
-#                 s.connect((self.__host, self.__port))  # 绑定地址和端口,必须跟客户端一样
-#             except Exception as e:
-#                 print('occur exception {}'.format(e))
-#             #data_res = data_res+str(i)
-#             rand=random.randint(1,10)
-#             data_res ="insert into tbl values ({},\'{}\')".format(rand,self.dic[str(rand)])
-#             self.fnSockSendMsg(s,data_res)
-#             print(f"{i}发送数据成功{data_res}")
-#             #s.sendall(data_res.encode())  # 发送信息
-#         #self.fnSockSendMsg(s, 'close')
-#             s.close()
-#     def send_quit_message(self):
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         try:
-#             s.connect(('127.0.0.1', 5555))  # 绑定地址和端口,必须跟客户端一样
-#         except Exception as e:
-#             print('occur exception {}'.format(e))
-#         self.fnSockSendMsg(s,'close')
-#         s.close()
-#
-#     def fnSockSendMsg(self, conn, msg):
-#         msg = msg.encode('utf-8')
-#         dataHead = struct.pack('i', len(msg))  # 把消息长度打包成固定长度的bytes
-#         conn.send(dataHead)  # 发送包头
-#         conn.send(msg)  # 发送消息
-#
-#         # 获取消息，处理粘包
-#
-#     def fnSockGetMsg(self, conn):
-#         dataHead = conn.recv(4)  # 先获取包头
-#         dataLen = struct.unpack('i', dataHead)[0]  # 把包头解包成int类型
-#         # 开始读取长度为 dataLen 的数据
-#         recData = b''  # 获取到的消息
-#         cacheLen = 1024  # 缓冲区大小
-#         page = dataLen // cacheLen  # 分片
-#         if page > 0:
-#             for i in range(page):
-#                 recData += conn.recv(cacheLen)
-#             pageEnd = dataLen % cacheLen  # 最后一片
-#             recData += conn.recv(pageEnd)
-#         else:
-#             recData += conn.recv(dataLen)
-#         msg = recData.decode('utf-8')
-#         return msg
-
-# def fnSockGetMsg( conn):
-#     dataHead = conn.recv(4)  # 先获取包头
-#     dataLen = struct.unpack('i', dataHead)[0]  # 把包头解包成int类型
-#         # 开始读取长度为 dataLen 的数据
-#     recData = b''  # 获取到的消息
-#     cacheLen = 1024  # 缓冲区大小
-#     page = dataLen // cacheLen  # 分片
-#     if page > 0:
-#         for i in range(page):
-#             recData += conn.recv(cacheLen)
-#         pageEnd = dataLen % cacheLen  # 最后一片
-#         recData += conn.recv(pageEnd)
-#     else:
-#         recData += conn.recv(dataLen)
-#     msg = recData.decode('utf-8')
-#     return msg
 if __name__ == '__main__':
-    client = Client(host='127.0.0.1',port=3333)
-    client.send_message_num(10)
-    #client.send_message(10)
-    # m =random.randint(10,1000)
-    # n =random.randint(10,1000)
-    # print(m,n)
-    # t1=threading.Thread(target=client.send_message,args=(m,))
-    # t2 = threading.Thread(target=client.send_message, args=(n,))
-    # t1.start()
-    # t2.start()
-    # t1.join()
-    # t2.join()
-    # client.send_quit_message()
-    # print('wait exit')
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.bind(('127.0.0.1', 6666))
-    # s.listen()
-    # while True:
-    #     try:
-    #         connection, address = s.accept()
-    #         data = fnSockGetMsg(connection)
-    #         if data == 'close':
-    #             break
-    #     except Exception as e:
-    #         print('occur exception as {}'.format(e))
-    #         break
-    # print('exit')
+    client = Client(host='127.0.0.1', port=3333)
+    # connection=client.create_socket(bind=False)
+    l1=[]
+    l2=[]
+    for i in range (100):
+        l1.append(str(i))
+        l2.append(str(i*2))
+    #client.send_message_from_list(connection=None,msgs=l1,close=True)
+    #client.send_message_from_list(connection=None, msgs=l1, close=True)
+    t1 = threading.Thread(target=client.send_message_from_list, args=(None, l1, True))
+    t2 = threading.Thread(target=client.send_message_from_list, args=(None, l2, True))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print('finished')
+    # s=client.create_socket(bind=False)
+    # for i in range(100):
+    #     client.send_message_from_list(connection=s,msgs=[str(i)],close=False)
+    #     client.send_message_from_list(connection=s,msgs=[str(i*2)],close=False)
